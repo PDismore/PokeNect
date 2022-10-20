1dc3d0cabe27e7cdcc6f7a7c0571649ac57e5c56
Reece Devenney <reecedevenney@gmail.com>
2022-10-19 23:07:26 -0400

    fixed db not updating

--- models/User.js
+++ models/User.js
@@ -1,2 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+
+  module.exports = User;
