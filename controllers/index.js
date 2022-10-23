81dfda89ec0b7fbf93e6feff12d20dea10071367
Reece Devenney <reecedevenney@gmail.com>
2022-10-23 19:30:49 -0400

    added homepage routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./homepage-rotues')
+
 router.use('/api', apiRoutes);
+router.use('/homepage', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
