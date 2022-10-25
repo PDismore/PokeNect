966c63c6e353a7d0805fe356a51954776926ccb2
richielollis <richiedlollis@gmail.com>
2022-10-24 21:17:21 -0400

    bug fix for server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
-const commentRoutes = require('./comment-routes.js');
-const bioRoutes = require('./bio-routes.js');
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes.js");
+const commentRoutes = require("./comment-routes.js");
 
-router.use('/user', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
-router.use('/bio', bioRoutes);
+router.use("/user", userRoutes);
+router.use("/post", postRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
